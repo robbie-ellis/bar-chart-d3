2a3b7495128b96d354ebffcbcb5cb3cff3e4cfb6
robbie-ellis <128123325+robbie-ellis@users.noreply.github.com>
2024-08-07 13:23:54 -0700

    added fetch data and started implementing D3

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-"//Let's get started" 
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+    .then(response => response.json())
+    .then(response => {
+      const dataSet = response.data;
+      
+      let dates = [];
+      let values = [];
+
+      dataSet.forEach((el) => {
+        dates.push(el[0]);
+        values.push(el[1]);
+      });
+
+      d3.select('body').selectAll('div')
+        .data(dataSet)
+        .enter()
+        .append('div')
+        .attr("class", "bar");
+      
+    });
+    
+    
+});
+
+
