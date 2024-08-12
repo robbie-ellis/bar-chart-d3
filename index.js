95f7ea9a01122159e1d966cd1c3ba8cfadc42db8
robbie-ellis <128123325+robbie-ellis@users.noreply.github.com>
2024-08-12 16:29:01 -0700

    Added tooltip functionality with dynamic data values

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const w = 850;
       const h = 400;
 
-      const svg = d3.select('body')
+      const svg = d3.select('#chart')
                     .append('svg')
                     .attr('height', h)
                     .attr('width', w);
@@ -34,24 +34,49 @@ document.addEventListener('DOMContentLoaded', () => {
         .attr('data-gdp', (d) => d)
         .data(dates)
         .attr('data-date', (d) => d);
-        
-      /* For testing  
-      const el = document.getElementsByClassName('bar')[0];
-      const output = el.getAttribute('data-gdp');
-      console.log(output);
-      */
-      const showToolTip = (e) => {
-        console.log("made it this far");
-        /*
-        d3.select('#tool-tip')
-        .data(dates);
-        */
-      }
+
+      const toolTip = d3.select('#tooltip'); 
+
+      const showToolTip = function() {
+        toolTip.attr('data-date', d3.select(this).attr('data-date'));
+        const dateFull = toolTip.attr('data-date');
+        console.log(dateFull);
+        const gdp = d3.select(this).attr('data-gdp');
+        const barX = parseInt(d3.select(this).attr('x'), 10);
+        let date;
+
+        const dateSplits = dateFull.split("-");
+        switch (dateSplits[1]) {
+          case "01":
+            date = dateSplits[0] + " Q1";
+            break;
+          case "04":
+            date = dateSplits[0] + " Q2";
+            break;
+          case "07":
+            date = dateSplits[0] + " Q3";
+            break;
+          case "10":
+            date = dateSplits[0] + " Q4";
+            break;
+          default:
+            //Default should never occur
+        };
+
+        toolTip.style('opacity', 0.8);
+        toolTip.style('left', (barX - 30) + 'px');
+        toolTip.html(`${date}<br>$${gdp}B`);
+      };
+
+      const hideToolTip = () => {
+        toolTip.style('opacity', 0);
+      };
 
       const bars = document.getElementsByClassName('bar');
       console.log(bars.length);
       for (let i = 0; i < bars.length; i++) {
         bars[i].addEventListener('mouseover', showToolTip);
+        bars[i].addEventListener('mouseout', hideToolTip);
       };
     });
     
