2a8adb4c6982d4e902df3b0ad62a9105d13b3276
robbie-ellis <128123325+robbie-ellis@users.noreply.github.com>
2024-08-19 19:10:04 -0700

    Fixed tooltip issues

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
       
       let dates = [];
       let values = [];
+      const w = 900;
+      const h = 600;
+      const padding = 40;
+      const toolTipHeight = 40;
+      const toolTipWidth = 80;
+      const textXOffset = 7; // Used for centering text horizontally in tooltip
+      const textYOffset = 16; // Used for centering text vertically in tooltip
 
       dataSet.forEach((el) => {
         dates.push(el[0]);
@@ -21,11 +28,11 @@ document.addEventListener('DOMContentLoaded', () => {
       dates.forEach((el, i) => {
         years[i] = el.substring(0, 4); 
       });
-    
-      const w = 900;
-      const h = 600;
-      const padding = 40;
 
+      /* 
+      Creates scales so rects and axes fit within 
+      the svg and rect values match axis values
+      */
       const xScale = d3.scaleLinear()
         .domain([d3.min(years), d3.max(years)])
         .range([padding, w - padding]);
@@ -38,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .domain([0, (3 * values.length -3)])
         .range([padding, w - padding]);
 
+      // Creates svg to which rectangles and axes will be added
       const svg = d3.select('#chart')
         .append('svg')
         .attr('height', h)
@@ -46,6 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .style('border-radius', '10px')
         .style('box-shadow', '10px 10px 5px 0px rgba(0, 0, 0, 0.5)');
 
+      // Generates rectangles   
       svg.selectAll('rect')
         .data(values)
         .enter()
@@ -53,14 +62,15 @@ document.addEventListener('DOMContentLoaded', () => {
         .attr('id', (d, i) => i)
         .attr('class', 'bar')
         .attr('x', (d, i) => rectXScale(i * 3))
-        .attr('y', (d) => yScale(d)) //'- 5' compensates for the 5 added to height
+        .attr('y', (d) => yScale(d))
         .attr('width', 3)
-        .style('height', (d) => h - padding - yScale(d))//Adding 5 to height to make smaller bars more visible
+        .style('height', (d) => h - padding - yScale(d))
         .attr('fill', 'purple')
         .attr('data-gdp', (d) => d)
         .data(dates)
         .attr('data-date', (d) => d);
 
+      // Adds Axes  
       const xAxis = d3.axisBottom(xScale)
         .tickFormat(d3.format('d'));
       const yAxis = d3.axisLeft(yScale);       
@@ -77,11 +87,22 @@ document.addEventListener('DOMContentLoaded', () => {
         .call(yAxis)
         .attr('color', 'coral');
 
-      const toolTip = d3.select('#tooltip'); 
+      // Adds tooltip functionality  
+      //const toolTip = d3.select('#tooltip'); 
+      let toolTip;
+      let toolTipTextDate;
+      let toolTipTextGDP;
 
       const showToolTip = function() {
-        toolTip.attr('data-date', d3.select(this).attr('data-date'));
-        const dateFull = toolTip.attr('data-date');
+        toolTip = svg.append('rect')
+          .attr('id', 'tooltip')
+          .attr('width', toolTipWidth)
+          .attr('height', toolTipHeight)
+          .attr('rx', 10)
+          .attr('ry', 10)
+          .attr('fill', 'coral')
+          .attr('data-date', d3.select(this).attr('data-date'));
+        const dateFull = toolTip.attr('data-date'); //Needs to be associated with tooltip to pass test
         const gdp = d3.select(this).attr('data-gdp');
         const barX = parseInt(d3.select(this).attr('x'), 10);
         let date;
@@ -104,23 +125,37 @@ document.addEventListener('DOMContentLoaded', () => {
             //Default should never occur
         };
 
-        toolTip.style('opacity', 0.8);
-        //toolTip.style('transform', 'translate(${barX}px, 250px)')
-        toolTip.style('left', (barX + 50) + 'px');
-        //toolTip.attr('x', )
-        toolTip.html(`${date}<br>$${gdp}B`);
+        toolTip.style('opacity', 0.8)
+        .attr('x', barX - toolTipWidth / 2)
+        .attr('y', h / 2 - toolTipHeight);
+        
+        toolTipTextDate = svg.append('text')
+          .text(`${date}$`)
+          .attr('x', barX - toolTipWidth /  2 + textXOffset)
+          .attr('y', h / 2 - toolTipHeight + textYOffset);
+          
+        toolTipTextGDP = svg.append('text')
+          .text(`${gdp}B`)
+          .attr('x', barX - toolTipWidth /  2 + textXOffset)
+          .attr('y', h / 2 - toolTipHeight + textYOffset * 2);
       };
-
+      
+      // Removes tooltip on mouseout
       const hideToolTip = () => {
-        toolTip.style('opacity', 0);
+        toolTip.remove();
+        toolTipTextDate.remove();
+        toolTipTextGDP.remove();
       };
 
+      // Sets up event listeners for tooltip functionality
       const bars = document.getElementsByClassName('bar');
       for (let i = 0; i < bars.length; i++) {
         bars[i].addEventListener('mouseover', showToolTip);
         bars[i].addEventListener('mouseout', hideToolTip);
       };
     });
+
+    
     
     
 });
